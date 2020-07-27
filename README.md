86f3a4fb77819eb4714831da01296a782a592bff
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-07-27 20:40:26 -0300

    8. Escrevendo nosso primeiro aplicativo completo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # curso-google-flutter
  Flutter: crie apps Android/iOS com novo SDK mobile do Google 
+
+
+# Para excutar o projeto 
+ir na pasta do flutter na raiz do seu disco rigido 
+encontrar o arquivo flutter_console
+excutar e navegar até o diretorio do projeto 
+após isso excutar o comando abaixo 
+<nome do emulador seu no caso o meu é o nexus>
+flutter emulators --lauch Nexus 5x API 29 x86
---
+++ hello_word/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new HelloWordScreen());
+}
+//StatelessWidget os eventos não sao tratados como telas estaticas
+class HelloWordScreen extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context) {
+       
+    return MaterialApp(
+       home:Scaffold(
+         appBar:AppBar(
+           title: Text("Cabeçalho do App "),
+         ),
+         body: Center(
+           child: Text("Conteudo do Body,  ")
+           ),
+         )
+       );
+  }
+  
+}
